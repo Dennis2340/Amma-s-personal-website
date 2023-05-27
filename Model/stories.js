15782049836e4592da958d4b0c044ce85690486e
Emmanuel Baimba <emmanuelleosamuel203@gmail.com>
2023-05-27 02:15:10 +0000

    fixing the date

--- Model/stories.js
+++ Model/stories.js
@@ -20,7 +20,7 @@ const storySchema = new Schema({
     },
     createdAt: {
         type: Date, 
-        default: format(new Date(), "MMMM-dd',' yyyy hh:mm aaa")
+        default: new Date().getTime()
     }
 })
 
