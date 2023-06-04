ffed60dfae6a90d7062a26f4e1325a4ace686f2c
Emmanuel Baimba <emmanuelleosamuel203@gmail.com>
2023-06-04 14:59:57 +0000

    token

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -15,8 +15,25 @@ const verifyToken = async (req, res, next) => {
 
     // Check if the token has expired
     const currentTime = Math.floor(Date.now() / 1000); // Current time in seconds
+    const expirationTime = 60; // Expiration time in seconds (1 minute)
+    
     if (verified.exp < currentTime) {
       return res.status(401).json({ msg: "Token has expired. Please login again." });
+    }
+
+    // Check if the token is within the expiration time
+    if (verified.exp - currentTime < expirationTime) {
+      // Refresh the token or perform any necessary action
+      // For example, you can issue a new token here
+      
+      // Set the new expiration time
+      verified.exp = currentTime + expirationTime;
+      
+      // Generate a new token with the updated expiration time
+      const newToken = jwt.sign(verified, process.env.JWT_SECRET);
+
+      // Set the new token in the response header or send it in the response body
+      res.setHeader("Authorization", `Bearer ${newToken}`);
     }
 
     next();
