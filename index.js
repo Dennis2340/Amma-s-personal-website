89cebb3ee228fac1c2ab86ff4841dea2ae87ac85
Emmanuel Baimba <emmanuelleosamuel203@gmail.com>
2023-06-04 13:20:10 +0000

    verifying the token in the routes

--- index.js
+++ index.js
@@ -37,30 +37,30 @@ app.delete("/poem/deletePoem/:id",verifyToken,poemController.deletePoem)
 app.put("/poem/updatePoem/:id", verifyToken,poemController.updatePoem)
    
 ///////// This is the Video Routes and RestApi //////////////////
-app.post("/addVideo",upload.single("video") ,videoController.create)
+app.post("/addVideo",upload.single("video") ,verifyToken,videoController.create)
 app.get("/getAllVideos", videoController.getAllVideos)
 app.get("/getSingleVideo/:id", videoController.getSingleVideo)
-app.delete("/deleteSingleVideo/:id", videoController.deleteVideo)
+app.delete("/deleteSingleVideo/:id", verifyToken,videoController.deleteVideo)
 
 ///////// This is the Story Routes and RestApi //////////////////
-app.post("/story/addStory", storyController.addNewStory);
+app.post("/story/addStory",verifyToken, storyController.addNewStory);
 app.get("/story/getAllStory", storyController.getAllStories);
 app.get("/story/getSingleStory/:id", storyController.getSingleStory) 
-app.delete("/story/deleteStory/:id", storyController.deleteStory)
-app.put("/story/updateStory/:id", storyController.updateStory)
+app.delete("/story/deleteStory/:id",verifyToken, storyController.deleteStory)
+app.put("/story/updateStory/:id",verifyToken, storyController.updateStory)
 
 ///////// This is the Article Routes and RestApi //////////////////
-app.post("/article/addArticle",articleController.addNewArticle);
+app.post("/article/addArticle",verifyToken, articleController.addNewArticle);
 app.get("/article/getAllArticles", articleController.getAllArticles);
 app.get("/article/getSingleArticle/:id", articleController.getSingleArticle) 
-app.delete("/article/deleteArticle/:id", articleController.deleteArticle)
-app.put("/article/updateArticle/:id", articleController.updateArticle)
+app.delete("/article/deleteArticle/:id",verifyToken, articleController.deleteArticle)
+app.put("/article/updateArticle/:id",verifyToken, articleController.updateArticle)
 
 ///////// This is the Article Routes and RestApi //////////////////
-app.post("/motMessage/addMotMessage", motMessageController.addNewMotMessage);
+app.post("/motMessage/addMotMessage",verifyToken, motMessageController.addNewMotMessage);
 app.get("/motMessage/getAllMotMessage", motMessageController.getAllMotMessage);
 app.get("/motMessage/getSingleMotMessage/:id", motMessageController.getSingleMotMessage) 
-app.delete("/motMessage/deleteMotMessage/:id", motMessageController.deleteMotMessage)
-app.put("/motMessage/updateMotMessage/:id", motMessageController.updateMotMessage)
+app.delete("/motMessage/deleteMotMessage/:id",verifyToken, motMessageController.deleteMotMessage)
+app.put("/motMessage/updateMotMessage/:id",verifyToken, motMessageController.updateMotMessage)
 
 app.listen(port, () => console.log(`Server is running at http://localhost:${port}`));
